d0cd9b082299ee8e965264c60b8ba6207553081b
Ezequiel Fonseca <ezequiel_fs13@hotmail.com>
2020-11-12 06:15:02 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/example/course/entities/User.java
+++ src/main/java/com/example/course/entities/User.java
@@ -1,13 +1,20 @@
 package com.example.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
@@ -19,6 +26,10 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	private List <Order> orders = new ArrayList<>(); 
 	
 	public User() {
 		
@@ -71,6 +82,10 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public List <Order> getOrders() {
+		return orders;
+	}
 
 	@Override
 	public int hashCode() {
@@ -95,11 +110,5 @@ public class User implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
-	}
-
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", name=" + name + ", email=" + email + ", phone=" + phone + ", password=" + password
-				+ "]";
 	}
 }
--- src/main/java/com/example/course/resources/OrderResource.java
+++ src/main/java/com/example/course/resources/OrderResource.java
@@ -9,26 +9,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.course.entities.User;
-import com.example.course.services.UserService;
+import com.example.course.entities.Order;
+import com.example.course.services.OrderService;
 
 @RestController
-@RequestMapping(value="/users") 
-public class UserResource {
+@RequestMapping(value="/orders") 
+public class OrderResource {
 	
 	@Autowired
-	private UserService service;
+	private OrderService service;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
-		List<User> users = service.findAll();
-		return ResponseEntity.ok().body(users);
+	public ResponseEntity<List<Order>> findAll(){
+		List<Order> orders = service.findAll();
+		return ResponseEntity.ok().body(orders);
 	}
 	
 	@GetMapping(value="/{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id){
-		User user = service.findById(id);
-		return ResponseEntity.ok().body(user);
+	public ResponseEntity<Order> findById(@PathVariable Long id){
+		Order order = service.findById(id);
+		return ResponseEntity.ok().body(order);
 	}
 }
-  
