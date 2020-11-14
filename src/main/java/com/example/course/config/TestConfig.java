e0970efbc319096e8f7a998ce9d5497625c5e7d8
Ezequiel Fonseca <ezequiel_fs13@hotmail.com>
2020-11-13 21:06:10 -0300

    Payment, one-to-one association

--- src/main/java/com/example/course/config/TestConfig.java
+++ src/main/java/com/example/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.example.course.entities.Category;
 import com.example.course.entities.Order;
 import com.example.course.entities.OrderItem;
+import com.example.course.entities.Payment;
 import com.example.course.entities.Product;
 import com.example.course.entities.User;
 import com.example.course.entities.enums.OrderStatus;
@@ -70,7 +71,7 @@ public class TestConfig implements CommandLineRunner{
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.SHIPPED, u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.CANCELED, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.PAID, u1); 
 		
@@ -80,9 +81,14 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
 		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
 		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
-		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T20:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.saveAll(Arrays.asList(o1));
 	}
 		
 }
